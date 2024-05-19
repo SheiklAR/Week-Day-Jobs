6b1ff6b295862daf89e96fadf58550ba2a71e584
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-05-19 19:03:47 +0530

    Added logic to filter jobs based on selected role and experience.

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,29 @@ import React, {useState} from 'react';
 import getSampleJdJSON from './jobData';
 import JobCard from './components/JobCard';
 import RoleFilter from './components/filters/RoleFilter';
+import ExperienceFilter from './components/filters/ExperienceFilter';
 
 const App = () => {
   const [selectedRole, setSelectedRole] = useState(null);
+  const [selectedExperience, setSelectedExperience] = useState(null);
 
   const jobData = getSampleJdJSON();
   
   let filteredJobs = jobData
 
   if (selectedRole) { 
-    filteredJobs = jobData.filter(job => job.jobRole.toLowerCase() === selectedRole.label.toLowerCase())
+    filteredJobs = filteredJobs.filter(job => job.jobRole.toLowerCase() === selectedRole.label.toLowerCase())
+  }
+
+  if (selectedExperience) { 
+    filteredJobs = filteredJobs.filter(job => job.minExp <= parseInt(selectedExperience.label))
   }
 
   return (
     <>
       <div className="filtersContainer">
-        <RoleFilter setSelectedRole={setSelectedRole}/>
+        <RoleFilter setSelectedRole={setSelectedRole} />
+        <ExperienceFilter setSelectedExperience={setSelectedExperience} />
       </div>  
       <div className="cardContainer">
         {filteredJobs.map(job =>
