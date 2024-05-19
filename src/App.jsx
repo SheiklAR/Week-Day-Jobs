425bae053ff1a1b49182e233f3547c4eab905ab2
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-05-19 18:14:08 +0530

    Added logic to filter jobs based on selected Roles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import getSampleJdJSON from './jobData';
 import JobCard from './components/JobCard';
+import RoleFilter from './components/filters/RoleFilter';
 
 const App = () => {
+  const [selectedRole, setSelectedRole] = useState(null);
+
   const jobData = getSampleJdJSON();
-  console.log(jobData)
+  
+  let filteredJobs = jobData
+
+  if (selectedRole) { 
+    filteredJobs = jobData.filter(job => job.jobRole.toLowerCase() === selectedRole.label.toLowerCase())
+  }
 
   return (
-    <div className="cardContainer">
-      {jobData.map(job =>
-      <JobCard key={job.jdUid} job={job} />
-      )}
-    </div>
+    <>
+      <div className="filtersContainer">
+        <RoleFilter setSelectedRole={setSelectedRole}/>
+      </div>  
+      <div className="cardContainer">
+        {filteredJobs.map(job =>
+          <JobCard key={job.jdUid} job={job} />
+        )}
+      </div>
+    </>
   );
 };
 
